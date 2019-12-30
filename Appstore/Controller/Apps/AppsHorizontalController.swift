2eae970df1695a54879dd9c16cb77b9ab8ea07ea
Cole Riggle <coleriggle@gmail.com>
2019-12-30 00:44:40 -0500

    Refined app display

--- Appstore/Controller/Apps/AppsHorizontalController.swift
+++ Appstore/Controller/Apps/AppsHorizontalController.swift
@@ -14,8 +14,8 @@ class AppsHorizontalController: BaseListController, UICollectionViewDelegateFlow
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .blue
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(AppRowCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.backgroundColor = .white
         
         if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
             layout.scrollDirection = .horizontal
@@ -28,7 +28,6 @@ class AppsHorizontalController: BaseListController, UICollectionViewDelegateFlow
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .red
         return cell
     }
     
@@ -39,7 +38,7 @@ class AppsHorizontalController: BaseListController, UICollectionViewDelegateFlow
         
         let height = (view.frame.height - 2 * topBottomPadding - 2 * lineSpacing) / 3
         
-        return .init(width: view.frame.width, height: height)
+        return .init(width: view.frame.width - 48, height: height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
---
+++ Appstore/Helpers/Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Extensions.swift
+//  Appstore
+//
+//  Created by Cole Riggle on 12/30/19.
+//  Copyright © 2019 Cole Riggle. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    convenience init(text: String, font: UIFont) {
+        self.init(frame: .zero)
+        self.text = text
+        self.font = font
+    }
+}
+
+extension UIImageView {
+    convenience init(cornerRadius: CGFloat) {
+        self.init(image: nil)
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+        self.contentMode = .scaleAspectFill
+    }
+}
+
+extension UIButton {
+    convenience init(title: String) {
+        self.init(type: .system)
+        self.setTitle(title, for: .normal)
+        
+    }
+}
