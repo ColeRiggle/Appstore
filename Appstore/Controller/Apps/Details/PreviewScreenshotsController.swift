84bd6f3f5f3484d537da3b9a9c591bd254557ca5
Cole Riggle <coleriggle@gmail.com>
2020-01-16 22:23:18 -0500

    Added review stars and JSON support for ratings

--- Appstore/Controller/Apps/Details/PreviewScreenshotsController.swift
+++ Appstore/Controller/Apps/Details/PreviewScreenshotsController.swift
@@ -38,7 +38,7 @@ class PreviewScreenshotsController: HorizontalSnappingController, UICollectionVi
         super.viewDidLoad()
         collectionView.backgroundColor = .white
         collectionView.register(ScreenshotCell.self, forCellWithReuseIdentifier: cellId)
-        collectionView.contentInset = .init(top: 0, left: 16, bottom: 16, right: 16)
+        collectionView.contentInset = .init(top: 0, left: 16, bottom: 0, right: 16)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
