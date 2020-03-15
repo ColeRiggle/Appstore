f47ea5dc0fbe85f4afe842e359977091511f6d15
Cole Riggle <coleriggle@gmail.com>
2020-03-14 23:54:32 -0400

    Changes to pod file confirguration and bugs when dismissing Today view controller

--- Appstore/Controller/Today/AppFullscreenController.swift
+++ Appstore/Controller/Today/AppFullscreenController.swift
@@ -1,48 +1,59 @@
 //
 //  AppFullscreenController.swift
-//  Appstore
+//  AppStoreJSONApis
 //
-//  Created by Cole Riggle on 2/16/20.
-//  Copyright © 2020 Cole Riggle. All rights reserved.
+//  Created by Brian Voong on 2/28/19.
+//  Copyright © 2019 Brian Voong. All rights reserved.
 //
 
 import UIKit
 
 class AppFullscreenController: UITableViewController {
+    
+    var dismissHandler: (() ->())?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Get rid of excess lines
         tableView.tableFooterView = UIView()
         tableView.separatorStyle = .none
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        return 2
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.item == 0 {
+            let headerCell = AppFullscreenHeaderCell()
+            headerCell.closeButton.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+            return headerCell
+        }
+        
         let cell = AppFullscreenDescriptionCell()
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = TodayCell()
-        return header
+    @objc fileprivate func handleDismiss(button: UIButton) {
+        button.isHidden = true
+        dismissHandler?()
     }
     
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 450
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 450
+        }
+        return super.tableView(tableView, heightForRowAt: indexPath)
     }
     
-    init() {
-        
-        // Make the entire table view scrollable, including the header
-        super.init(style: .grouped)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+//    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let header = TodayCell()
+//        return header
+//    }
+//
+//    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return 450
+//    }
     
 }
--- Appstore/Controller/Today/TodayController.swift
+++ Appstore/Controller/Today/TodayController.swift
@@ -1,9 +1,9 @@
 //
 //  TodayController.swift
-//  Appstore
+//  AppStoreJSONApis
 //
-//  Created by Cole Riggle on 2/6/20.
-//  Copyright © 2020 Cole Riggle. All rights reserved.
+//  Created by Brian Voong on 2/27/19.
+//  Copyright © 2019 Brian Voong. All rights reserved.
 //
 
 import UIKit
@@ -17,50 +17,85 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
         
         navigationController?.isNavigationBarHidden = true
         
-        collectionView.backgroundColor = .systemGray6
+        collectionView.backgroundColor = #colorLiteral(red: 0.948936522, green: 0.9490727782, blue: 0.9489068389, alpha: 1)
         
         collectionView.register(TodayCell.self, forCellWithReuseIdentifier: cellId)
     }
     
-    var appFullscreenController: UIViewController!
+    var appFullscreenController: AppFullscreenController!
+    
+    var topConstraint: NSLayoutConstraint?
+    var leadingConstraint: NSLayoutConstraint?
+    var widthConstraint: NSLayoutConstraint?
+    var heightConstraint: NSLayoutConstraint?
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
         let appFullscreenController = AppFullscreenController()
-
+        appFullscreenController.dismissHandler = {
+            self.handleRemoveRedView()
+        }
         let redView = appFullscreenController.view!
         redView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleRemoveRedView)))
         view.addSubview(redView)
-        
+
         addChild(appFullscreenController)
+        
         self.appFullscreenController = appFullscreenController
         
-        guard let cell = collectionView.cellForItem(at: indexPath) else
-            { return }
+        guard let cell = collectionView.cellForItem(at: indexPath) else { return }
         
-        // absolute coordinates of cell
-        guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else
-            { return }
+        // absolute coordindates of cell
+        guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else { return }
         
         self.startingFrame = startingFrame
+
+        // auto layout constraint animations
+        // 4 anchors
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        topConstraint = redView.topAnchor.constraint(equalTo: view.topAnchor, constant: startingFrame.origin.y)
+        leadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: startingFrame.origin.x)
+        widthConstraint = redView.widthAnchor.constraint(equalToConstant: startingFrame.width)
+        heightConstraint = redView.heightAnchor.constraint(equalToConstant: startingFrame.height)
         
-        redView.frame = startingFrame
+        [topConstraint, leadingConstraint, widthConstraint, heightConstraint].forEach({$0?.isActive = true})
+        self.view.layoutIfNeeded()
+        
         redView.layer.cornerRadius = 16
         
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
-            redView.frame = self.view.frame
-            self.tabBarController?.tabBar.isHidden = true
+
+            self.topConstraint?.constant = 0
+            self.leadingConstraint?.constant = 0
+            self.widthConstraint?.constant = self.view.frame.width
+            self.heightConstraint?.constant = self.view.frame.height
+            
+            self.view.layoutIfNeeded() // starts animation
+
+            self.tabBarController?.tabBar.transform = CGAffineTransform(translationX: 0, y: 100)
+
         }, completion: nil)
     }
     
     var startingFrame: CGRect?
     
-    @objc func handleRemoveRedView(gesture: UITapGestureRecognizer) {
-        // access startingFrame
+    @objc func handleRemoveRedView() {
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
-            gesture.view?.frame = self.startingFrame ?? .zero
-            self.tabBarController?.tabBar.isHidden = false
+            
+            self.appFullscreenController.tableView.contentOffset = .zero
+            
+            guard let startingFrame = self.startingFrame else { return }
+            self.topConstraint?.constant = startingFrame.origin.y
+            self.leadingConstraint?.constant = startingFrame.origin.x
+            self.widthConstraint?.constant = startingFrame.width
+            self.heightConstraint?.constant = startingFrame.height
+            
+            self.view.layoutIfNeeded()
+            
+            self.tabBarController?.tabBar.transform = .identity
+            
         }, completion: { _ in
-            gesture.view?.removeFromSuperview()
+            self.appFullscreenController.view.removeFromSuperview()
             self.appFullscreenController.removeFromParent()
         })
     }
@@ -83,6 +118,7 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return .init(top: 32, left: 0, bottom: 0, right: 0)
+        return .init(top: 32, left: 0, bottom: 32, right: 0)
     }
+    
 }
